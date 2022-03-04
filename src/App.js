587f304e6411ecb9638ffd05ff2b6cd1f025a792
ravikiran <ravikiransjce.jobs@gmail.com>
2022-03-04 11:44:11 +0530

    added redux store, need to get it working

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 class App extends React.Component {
   constructor() {
     super();
@@ -77,36 +80,38 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='grid-container'>
-        <header>
-          <a href='/'>React Shopping Cart</a>
-        </header>
-        <main>
-          <div className='content'>
-            <div className='main'>
-              <Filter
-                count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
-              ></Filter>
-              <Products
-                products={this.state.products}
-                addToCart={this.addToCart}
-              ></Products>
+      <Provider store={store}>
+        <div className='grid-container'>
+          <header>
+            <a href='/'>React Shopping Cart</a>
+          </header>
+          <main>
+            <div className='content'>
+              <div className='main'>
+                <Filter
+                  count={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}
+                ></Filter>
+                <Products
+                  products={this.state.products}
+                  addToCart={this.addToCart}
+                ></Products>
+              </div>
+              <div className='sidebar'>
+                <Cart
+                  cartItems={this.state.cartItems}
+                  removeFromCart={this.removeFromCart}
+                  createOrder={this.createOrder}
+                ></Cart>
+              </div>
             </div>
-            <div className='sidebar'>
-              <Cart
-                cartItems={this.state.cartItems}
-                removeFromCart={this.removeFromCart}
-                createOrder={this.createOrder}
-              ></Cart>
-            </div>
-          </div>
-        </main>
-        <footer>All Rights Reserved</footer>
-      </div>
+          </main>
+          <footer>All Rights Reserved</footer>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,52 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions/productActions";
 
-export default class Products extends Component {
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null,
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchProducts();
+  }
+
   render() {
     return (
       <div>
-        <ul className='products'>
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className='product'>
-                <a href={"#" + product._id}>
-                  <img src={product.image} alt={product.title} />
-                  <p>{product.title}</p>
-                </a>
-                <div className='product-price'>
-                  <div>${product.price}</div>
-                  <button
-                    onClick={() => this.props.addToCart(product)}
-                    className='button primary'
-                  >
-                    Add To Cart
-                  </button>
+        {!this.props.products ? (
+          <div>Loading</div>
+        ) : (
+          <ul className='products'>
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <div className='product'>
+                  <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title} />
+                    <p>{product.title}</p>
+                  </a>
+                  <div className='product-price'>
+                    <div>${product.price}</div>
+                    <button
+                      onClick={() => this.props.addToCart(product)}
+                      className='button primary'
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
 }
+
+export default connect((state) => ({ products: state.products.items }), {
+  fetchProducts,
+})(Products);
